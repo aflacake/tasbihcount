8cc7fe47625887ca0faa4e35a4888b7f54ed04f8
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-02-09 14:40:31 +0700

    Update script.js
    fix cost

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 //mengambil id
+const button1 =
+  document.querySelector("#buttuon1");
+  
 const count =
   document.querySelector("#count");
 
-const button1 =
-  document.querySelector("#buttuon1");
-
-const couted =
+const counted =
   document.querySelector("#counted");
 
 //jumlah terhitung
-  let cost = 0;
+let cost = 0;
+count.innerHTML = cost;
+
+function func1() {
+  cost = cost + 1;
   count.innerHTML = cost;
 
-  function jmlh() {
-    cost = cost + 1;
-    count.innerHTML = cost;
+if (cost === 33) {
+  counted.innerHTML = "Terhitug 33";
+ };
+if (cost === 66) {
+  counted.innerHTML = "Terhitung 66";
+ };
+if (cost === 99) {
+  counted.innerHTML = "Terhitung 99";
+ };
+if (cost === 100) {
+  counted.innerHTML = "Terhitung 100"
+ };
+};
 
-     if (cost === 33) {
-      counted.innerHTML = "Terhitug 33"
-    };
-     if (cost === 66) {
-      counted.innerHTML = "Terhitung 66"
-    };
-     if (cost === 99) {
-      counted.innerHTML = "Terhitung 99"
-     };
-  };
-
-button1.onClick = jmlh;
+button1.onclick = func1;
