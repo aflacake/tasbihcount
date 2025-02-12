7b1cab92c7e5c57649d0cd60b7efef2e3881359a
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-02-12 14:46:19 +0700

    Update script.js
    Pembaruan besar!

--- script.js
+++ script.js
@@ -28,13 +28,16 @@ if (cost === 99) {
   terhitung.innerHTML = "Terhitung 99. Wahai orang-orang beriman, ingatlah Allah dengan zikir sebanyak-banyaknya. (Q.S. Al-Ahzab: 41)"
  };
 if (cost === 100) {
-  terhitung.innerHTML = "Terhitung 100"
+  terhitung.innerHTML = "Terhitung 100. Langit yang tujuh, bumi dan semua yang ada di dalamnya bertasbih kepada Allah. Dan tidak ada sesuatu pun melainkan bertasbih dengan memuji-Nya, tetapi kamu tidak mengerti tasbih mereka. Sungguh Dia Maha Penyantun, Maha Pengampun (Q.S. Al-Isra': 44)"
  };
  if (cost === 1000) {
   terhitung.innerHTML = "Terhitung 1000. Wahai orang-orang beriman, ingatlah Allah dengan zikir sebanyak-banyaknya. (Q.S. Al-Ahzab: 41)"
  };
  if (cost === 3000) {
   terhitung.innerHTML = "Terhitung 3000. Wahai orang-orang beriman, ingatlah Allah dengan zikir sebanyak-banyaknya. (Q.S. Al-Ahzab: 41)"
+ };
+ if (cost === 99999) {
+  terhitung.innerHTML = "Terhitung 99999. Berkat rahmat-Nya, Dia jadikan untukmu malam dan siang agar kamu beristirahat pada malam hari, agar kamu mencari sebagian karunia-Nya (pada siang hari), dan agar kamu bersyukur kepada-Nya (Q.S. Al-Qashash: 73)"
  };
 };
 
