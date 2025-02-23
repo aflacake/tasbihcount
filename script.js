3628fe525c8c1f9b211b7a366af570c6455fab87
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-02-23 11:13:25 +0700

    Update script.js
    lines dan DOM

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
 //mengambil id
-const but =
-  document.querySelector(
-    "#but");
-  
-const hitung =
-  document.querySelector(
-    "#hitung");
-
-const terhitung =
-  document.querySelector(
-    "#terhitung");
+const but = document.querySelector("#but");
+const hitung = document.querySelector("#hitung");
+const terhitung = document.querySelector("#terhitung");
 
 //jumlah terhitung
 let cost = 0;
-hitung.innerHTML = cost;
+hitung.textContent = cost;
+const maxCost = 100000;
 
 function raz() {
+  if(cost < maxCost) {
   cost = cost + 1;
   hitung.innerHTML = cost;
+  }
+
 if (cost === 33) {
   terhitung.innerHTML = "Terhitung 33. Maka bersabarlah engkau (Muhammad) terhadap apa yang mereka katakan dan bertasbihlah dengan memuji Tuhanmu sebelum matahari terbit dan sebelum terbenam. (Q.S. Qaf: 39)";
  };
@@ -38,7 +35,8 @@ if (cost === 100) {
  };
  if (cost === 99999) {
   terhitung.innerHTML = "Terhitung 99999. Berkat rahmat-Nya, Dia jadikan untukmu malam dan siang agar kamu beristirahat pada malam hari, agar kamu mencari sebagian karunia-Nya (pada siang hari), dan agar kamu bersyukur kepada-Nya (Q.S. Al-Qashash: 73)"
- };
-};
+ }
+}
 
 but.onclick = raz;
+});
