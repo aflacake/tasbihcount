11fb322a817e8deff01ce711df3037dd223b2c20
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-04-13 10:05:29 +0700

    Potensi spam
    Tombol keamanan

--- acaraEid/demoidulfitriscript.js
+++ acaraEid/demoidulfitriscript.js
@@ -1,103 +1,102 @@
 document.addEventListener("DOMContentLoaded", function() {
 //mengambil id
-const but = document.querySelector("#but");
+const butEid = document.querySelector("#but");
 const hitungEid = document.querySelector("#hitung");
 const terhitungEid = document.querySelector("#terhitung");
 const resetBtnEid = document.querySelector("#reset");
 
 //jumlah terhitung
 let jumlahDemoEid = parseInt(localStorage.getItem('jumlahDemoEid')) || 0;
-hitung.textContent = jumlahDemoEid;
+hitungEid.textContent = jumlahDemoEid;
 const maxCost = 34;
 
 function raz() {
   if(jumlahDemoEid < maxCost) {
   jumlahDemoEid++;
-  hitung.textContent = jumlahDemoEid;
+  hitungEid.textContent = jumlahDemoEid;
   localStorage.setItem('jumlahDemoEid', jumlahDemoEid);
   }
 
   if (jumlahDemoEid === 1 && !localStorage.getItem("1")) {
-  terhitung.textContent = "Allahuakbar";
+  terhitungEid.textContent = "Allahuakbar";
  };
   if (jumlahDemoEid === 3 && !localStorage.getItem("3")) {
-  terhitung.textContent = "Alhamdulillah"
+  terhitungeEid.textContent = "Alhamdulillah"
  };
   if (jumlahDemoEid === 4 && !localStorage.getItem("4")) {
-  terhitung.textContent = "Allahuakbar"
+  terhitungEid.textContent = "Allahuakbar"
  };
   if (jumlahDemoEid === 6 && !localStorage.getItem("6")) {
-  terhitung.textContent = "Alhamdulillah"
+  terhitungEid.textContent = "Alhamdulillah"
  };
   if (jumlahDemoEid === 7 && !localStorage.getItem("7")) {
-  terhitung.textContent = "Allahuakbar"
+  terhitungEid.textContent = "Allahuakbar"
  };
   if (jumlahDemoEid === 9 && !localStorage.getItem("9")) {
-  terhitung.textContent = "Alhamdulillah"
+  terhitungEid.textContent = "Alhamdulillah"
  };
   if (jumlahDemoEid === 10 && !localStorage.getItem("10")) {
-  terhitung.textContent = "Allahuakbar"
+  terhitungEid.textContent = "Allahuakbar"
  };
   if (jumlahDemoEid === 12 && !localStorage.getItem("12")) {
-  terhitung.textContent = "Alhamdulillah"
+  terhitungEid.textContent = "Alhamdulillah"
  };
   if (jumlahDemoEid === 13 && !localStorage.getItem("13")) {
-  terhitung.textContent = "Allahuakbar"
+  terhitungEid.textContent = "Allahuakbar"
  };
   if (jumlahDemoEid === 15 && !localStorage.getItem("15")) {
-  terhitung.textContent = "Alhamdulillah"
+  terhitungEid.textContent = "Alhamdulillah"
  };
  if (jumlahDemoEid === 16 && !localStorage.getItem("16")) {
-  terhitung.textContent = "Allahuakbar"
+  terhitungEid.textContent = "Allahuakbar"
  };
  if (jumlahDemoEid === 18 && !localStorage.getItem("18")) {
-  terhitung.textContent = "Alhamdulillah"
+  terhitungEid.textContent = "Alhamdulillah"
  };
  if (jumlahDemoEid === 19 && !localStorage.getItem("19")) {
-  terhitung.textContent = "Allahuakbar"
+  terhitungEid.textContent = "Allahuakbar"
  };
  if (jumlahDemoEid === 21 && !localStorage.getItem("21")) {
-  terhitung.textContent = "Alhamdulillah"
+  terhitungEid.textContent = "Alhamdulillah"
  };
  if (jumlahDemoEid === 22 && !localStorage.getItem("22")) {
-  terhitung.textContent = "Allahuakbar"
+  terhitungEid.textContent = "Allahuakbar"
  };
  if (jumlahDemoEid === 24 && !localStorage.getItem("24")) {
-  terhitung.textContent = "Alhamdulillah"
+  terhitungEid.textContent = "Alhamdulillah"
  };
  if (jumlahDemoEid === 25 && !localStorage.getItem("25")) {
-  terhitung.textContent = "Allahuakbar"
+  terhitungEid.textContent = "Allahuakbar"
  };
  if (jumlahDemoEid === 27 && !localStorage.getItem("27")) {
-  terhitung.textContent = "Alhamdulillah"
+  terhitungEid.textContent = "Alhamdulillah"
  };
  if (jumlahDemoEid === 28 && !localStorage.getItem("28")) {
-  terhitung.textContent = "Allahuakbar"
+  terhitungEid.textContent = "Allahuakbar"
  };
  if (jumlahDemoEid === 30 && !localStorage.getItem("30")) {
-  terhitung.textContent = "Alhamdulillah"
+  terhitungEid.textContent = "Alhamdulillah"
  };
  if (jumlahDemoEid === 31 && !localStorage.getItem("31")) {
-  terhitung.textContent = "Allahuakbar"
+  terhitungEid.textContent = "Allahuakbar"
  };
  if (jumlahDemoEid === 33 && !localStorage.getItem("33")) {
-  terhitung.textContent = "Alhamdulillah"
+  terhitungEid.textContent = "Alhamdulillah"
  };
 };
 
-but.onclick = raz;
+butEid.onclick = raz;
 
 
 
 function reset() {
   jumlahDemoEid = 0;
-  hitung.innerHTML = jumlahDemoEid;
+  hitungEid.innerHTML = jumlahDemoEid;
     terhitung.textContent = '';
     localStorage.removeItem('jumlahDemoEid', jumlahDemoEid);
       console.log("Reset: " + jumlahDemoEid)
 }
     resetBtnEid.onclick = reset;
-});
 
 
 
@@ -130,3 +129,21 @@ function tampilKutipanAcakDemoEid() {
         }, waktuKutipAcakDemoEid);
     }
     setKutipanDemoIdulFitriRandom();
+
+
+
+
+// keamanan tombol
+let setelahWaktuKlikEid = 0;
+const klikThresholdEid = 2000;
+
+butEid.addEventListener('click', function(event) {
+	const waktuSaatIniEid = Date.now();
+	if (waktuSaatIniEid - setelahWaktuKlikEid < klikThresholdEid) {
+		alert('Klik sangat cepat! potensi spam');	
+	} else {
+		console.log('Klik valid!');
+	}
+	setelahWaktuKlikEid = waktuSaatIniEid;
+ });
+});
