1eed49f6ff38e7f31248e98dad9063c097a09d15
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-05-06 08:59:06 +0700

    Pembenaran indensasi di _cache_

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
 self.addEventListener('install', (event) => {
-	event.waitUntil(
-		caches.open('v1').then((cache) => {
-			return cache.addAll([
-				'index.html',
-				'style.css',
-				'script.js',
-				'https://raw.githubusercontent.com/aflacake/tasbihcount/main/img/logo-tasbih-count.ico',
-				'https://github.com/aflacake/tasbihcount/raw/main/img/logo-tasbih-count.png'
-			]);
-		})
-	);
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                'index.html',
+                'style.css',
+                'script.js',
+                'https://raw.githubusercontent.com/aflacake/tasbihcount/main/img/logo-tasbih-count.ico',
+                'https://github.com/aflacake/tasbihcount/raw/main/img/logo-tasbih-count.png'
+            ]);
+        })
+    );
 });
 
 self.addEventListener("push", (event) => {
